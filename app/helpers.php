3144ea7f5aca21f852e0abfec8f3c76a00a94ee7
Your Nameboomboomlei <1846352100@qq.com>
2019-05-23 05:43:53 +0000

    基础页面结构

--- app/helpers.php
+++ app/helpers.php
@@ -4,5 +4,8 @@
  * @Author: xuebingsi
  * @Date:   2019-05-23 10:52:34
  * @Last Modified by:   zhibinm
- * @Last Modified time: 2019-05-23 11:00:19
+ * @Last Modified time: 2019-05-23 11:19:26
  */
+function route_class(){
+	return str_replace('.','-',Route::currentRouteName());
+}
