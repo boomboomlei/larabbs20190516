0c869d41239ed240f5e3a29953cc55d752260149
Your Nameboomboomlei <1846352100@qq.com>
2019-05-24 08:47:54 +0000

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,11 +28,14 @@ class UserRequest extends FormRequest
             'name'=> 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,'.Auth::id(),
             'email'=>'required|email',
             'introduction'=>'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=288,min_height=208',
         ];
     }
 
     public function messages(){
         return [
+            'avatar.mimes'=>'头像必须是jpeg,bmp,png,gif 格式的图片',
+            'avatar.dimensions'=>'图片的清晰度不够，宽和高需要208px+',
             'name.unique'=>'用户名已经被占用，请换一个',
             'name.regex'=>'用户名支支持英文数字下划线横杠',
             'name.between'=>'用户名必须介于3-25个字符之间',
